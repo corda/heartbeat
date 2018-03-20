d352a5c8616cc7164c0594d0ff6b59ea867452f5
Joel Dudley <joeldudleyr3@users.noreply.github.com>
2018-03-20 11:24:21 +0000

    Adds RPC permissions.

--- src/test/kotlin/com/heartbeat/NodeDriver.kt
+++ src/test/kotlin/com/heartbeat/NodeDriver.kt
@@ -12,7 +12,7 @@ import net.corda.testing.node.User
  */
 fun main(args: Array<String>) {
     // No permissions required as we are not invoking flows.
-    val user = User("user1", "test", permissions = setOf())
+    val user = User("user1", "test", permissions = setOf("ALL"))
     driver(DriverParameters(isDebug = true, startNodesInProcess = true, waitForAllNodesToFinish = true)) {
         val partyA = startNode(providedName = CordaX500Name("PartyA", "London", "GB"), rpcUsers = listOf(user)).getOrThrow()
         startWebserver(partyA)
